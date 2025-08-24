3a98d161f63d2eaec486cc8af4685ee553b57384
Al-Amin14 <alaminj824@gmail.com>
2025-08-24 13:02:24 +0600

    merge resolve

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -91,7 +91,7 @@ const AuthPage = ({ type = 'login' }) => {
         if (!data.error) {
           localStorage.setItem("token", data.token);
           localStorage.setItem("userid", data.user.id);
-          return navigate(formData.role === "admin" ? "/admin/dashboard" : "/");
+          return navigate(formData.role === "Club Member" ? "/admin/dashboard" : "/");
           // return navigate(formData.role === "admin" ? "/admin/dashboard" : "/student/dashboard");
 
 
@@ -118,7 +118,7 @@ const AuthPage = ({ type = 'login' }) => {
 
       if (!data.error) {
         console.log("LKFLSDFSLFJLKSJD")
-        navigate(formData.role === "admin" ? "/admin/dashboard" : "/student/dashboard");
+        navigate(formData.role === "Club Member" ? "/admin/dashboard" : "/student/dashboard");
       } else {
         setErrors({ submit: data.error || "Authentication failed" });
       }
@@ -232,7 +232,7 @@ const AuthPage = ({ type = 'login' }) => {
                     className="mt-1 block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-[var(--primary-accent-1)] focus:border-transparent"
                   >
                     <option value="student">Student</option>
-                    <option value="admin">Club Member</option>
+                    <option value="Club Member">Club Member</option>
                   </select>
                 </div>
               </>
